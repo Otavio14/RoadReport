580a743fe685b43d0fa1bc6e98ac5625855bdae4
Otávio <79605860+Otavio14@users.noreply.github.com>
2021-03-22 10:46:01 -0300

    Animação do menu concluída.

--- app/src/main/java/com/otavio14/roadreport/MapsActivity.java
+++ app/src/main/java/com/otavio14/roadreport/MapsActivity.java
@@ -2,7 +2,10 @@ package com.otavio14.roadreport;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -12,10 +15,15 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    ExtendedFloatingActionButton efabSair, efabVerOcorrencias, efabRelatarOcorrencia, efabNomeUsuario;
+    FloatingActionButton fabMenuFechado;
+    boolean menuStatus = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fabMenuFechado = (FloatingActionButton) findViewById(R.id.menu_fechado);
+        efabVerOcorrencias = (ExtendedFloatingActionButton) findViewById(R.id.ver_ocorrencias);
+        efabSair = (ExtendedFloatingActionButton) findViewById(R.id.sair);
+        efabRelatarOcorrencia = (ExtendedFloatingActionButton) findViewById(R.id.relatar_ocorrencia);
+        efabNomeUsuario = (ExtendedFloatingActionButton) findViewById(R.id.nome_usuario);
+
+        fabMenuFechado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(menuStatus) {
+                    fabMenuFechado.setImageResource(R.drawable.ic_menu_fechado);
+                    fabMenuFechado.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.orange)));
+                    efabSair.hide();
+                    efabRelatarOcorrencia.hide();
+                    efabVerOcorrencias.hide();
+                    efabNomeUsuario.hide();
+                    menuStatus = false;
+                }
+                else {
+                    fabMenuFechado.setImageResource(R.drawable.ic_menu_aberto);
+                    fabMenuFechado.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.gray)));
+                    efabSair.show();
+                    efabRelatarOcorrencia.show();
+                    efabVerOcorrencias.show();
+                    efabNomeUsuario.show();
+                    menuStatus = true;
+                }
+            }
+        });
     }
 
     /**
