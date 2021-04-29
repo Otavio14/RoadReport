dd68ca58b00728a5ce8f3a184984fe57f721b11c
Otávio <79605860+Otavio14@users.noreply.github.com>
2021-04-29 10:18:15 -0300

    Implementados: validação de CPF, máscara de CPF e telefone, e senha com mínimo de 6 caracteres.

--- app/src/main/java/com/otavio14/roadreport/CadastroActivity.java
+++ app/src/main/java/com/otavio14/roadreport/CadastroActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +35,7 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Map;
 
@@ -46,6 +50,8 @@ public class CadastroActivity extends AppCompatActivity {
     FirebaseFirestore database = FirebaseFirestore.getInstance();
 
     public FirebaseAuth mAuth;
+
+    public static final String FORMAT_CPF = "###.###.###-##";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +72,50 @@ public class CadastroActivity extends AppCompatActivity {
         //Instance da conta
         mAuth = FirebaseAuth.getInstance();
 
+        //Formata o número de telefone
+        editTelefone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        editCpf.addTextChangedListener(new TextWatcher() {
+            boolean isUpdating;
+            String old = "";
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                final String str = unmask(s.toString());
+                String mascara = "";
+                if (isUpdating) {
+                    old = str;
+                    isUpdating = false;
+                    return;
+                }
+                int i = 0;
+                for (final char m : FORMAT_CPF.toCharArray()) {
+                    if (m != '#' && str.length() > old.length()) {
+                        mascara += m;
+                        continue;
+                    }
+                    try {
+                        mascara += str.charAt(i);
+                    } catch (final Exception e) {
+                        break;
+                    }
+                    i++;
+                }
+                isUpdating = true;
+                editCpf.setText(mascara);
+                editCpf.setSelection(mascara.length());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         buttonCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,8 +124,10 @@ public class CadastroActivity extends AppCompatActivity {
                         TextUtils.isEmpty(editEmail.getText().toString()) ||
                         TextUtils.isEmpty(editNome.getText().toString()) ||
                         TextUtils.isEmpty(editSenha.getText().toString()) ||
-                        TextUtils.isEmpty(editTelefone.getText().toString())) {
-                    Toast.makeText(getApplicationContext(), "Preencha todos os campos", Toast.LENGTH_LONG).show();
+                        TextUtils.isEmpty(editTelefone.getText().toString()) ||
+                        editSenha.getText().toString().length() < 6 ||
+                        validarCpf(unmask(editCpf.getText().toString())) == false) {
+                    Toast.makeText(getApplicationContext(), "Dados incorretos", Toast.LENGTH_LONG).show();
                 } else {
                     cadastrarConta();
                 }
@@ -89,6 +141,65 @@ public class CadastroActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private boolean validarCpf(String cpf) {
+        if (cpf.equals("00000000000") ||
+                cpf.equals("11111111111") ||
+                cpf.equals("22222222222") || cpf.equals("33333333333") ||
+                cpf.equals("44444444444") || cpf.equals("55555555555") ||
+                cpf.equals("66666666666") || cpf.equals("77777777777") ||
+                cpf.equals("88888888888") || cpf.equals("99999999999") ||
+                (cpf.length() != 11))
+            return(false);
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+            // Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i=0; i<9; i++) {
+                // converte o i-esimo caractere do CPF em um numero:
+                // por exemplo, transforma o caractere '0' no inteiro 0
+                // (48 eh a posicao de '0' na tabela ASCII)
+                num = (int)(cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig10 = '0';
+            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+
+            // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for(i=0; i<10; i++) {
+                num = (int)(cpf.charAt(i) - 48);
+                sm = sm + (num * peso);
+                peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig11 = '0';
+            else dig11 = (char)(r + 48);
+
+            // Verifica se os digitos calculados conferem com os digitos informados.
+            if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10)))
+                return(true);
+            else return(false);
+        } catch (InputMismatchException erro) {
+            return(false);
+        }
+    }
+
+    public static String unmask(final String s) {
+        return s.replaceAll("[.]", "").replaceAll("[-]", "").replaceAll("[/]", "").replaceAll("[(]", "").replaceAll("[ ]","").replaceAll("[:]", "").replaceAll("[)]", "");
     }
 
     /**
